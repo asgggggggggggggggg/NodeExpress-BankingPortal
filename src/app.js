cc8419334ae455ec50ce8e284b5304a806d538f8
Arindam Pal <M1006757@mindtree.com>
2019-05-31 08:42:42 +0530

    code commit
    code commit

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({extended : true}));
 
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'),'utf-8')
@@ -20,6 +21,27 @@ app.get('/savings', (req, res) => res.render('account', {account:accounts.saving
 app.get('/checking', (req, res) => res.render('account', {account:accounts.checking}));
 app.get('/credit', (req, res) => res.render('account', {account:accounts.credit}));
 
-app.get('/profile', (req, res) => res.render('profile', {user:users[0]}));
+app.get('/transfer', (req, res) => res.render('transfer'));
+
+app.post('/transfer', (req, res) =>{
+
+   accounts[req.body.from].balance =  accounts[req.body.from].balance - req.body.amount;
+   accounts[req.body.to].balance =  parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON),'utf-8')
+    res.render('tranfer',{message:'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment',{account:accounts.credit}));
+app.post('/payment', (req, res) =>{
+
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parserInt(req.bosy.amount, 10);
+     const accountsJSON = JSON.stringify(accounts, null, 4);
+     fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON),'utf-8')
+     res.render('payment',{message:'Payment Successfull', account:accounts.credit});
+ });
+
+app.get()
 
 app.listen(3000, () => console.log("PS project running on port 3000!"))

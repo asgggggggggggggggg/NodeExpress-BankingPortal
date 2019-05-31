b19dd56248aee2fb426d56e179d11b05e8b3f97d
Arindam Pal <M1006757@mindtree.com>
2019-05-31 08:54:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({extended : true}));
 
-
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'),'utf-8')
 const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'),'utf-8')
@@ -20,6 +19,13 @@ app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accou
 app.get('/savings', (req, res) => res.render('account', {account:accounts.savings}));
 app.get('/checking', (req, res) => res.render('account', {account:accounts.checking}));
 app.get('/credit', (req, res) => res.render('account', {account:accounts.credit}));
+
+app.get('/profile', (req, res) => res.render('profile', {user:users[0]}));
+
+
+
+
+
 
 app.get('/transfer', (req, res) => res.render('transfer'));
 
@@ -36,12 +42,11 @@ app.get('/payment', (req, res) => res.render('payment',{account:accounts.credit}
 app.post('/payment', (req, res) =>{
 
     accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parserInt(req.bosy.amount, 10);
+    accounts.credit.available += parseInt(req.body.amount, 10);
      const accountsJSON = JSON.stringify(accounts, null, 4);
      fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON),'utf-8')
      res.render('payment',{message:'Payment Successfull', account:accounts.credit});
  });
 
-app.get()
 
 app.listen(3000, () => console.log("PS project running on port 3000!"))

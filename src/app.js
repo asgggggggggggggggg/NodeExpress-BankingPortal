3ebbda5790f7a0a81fec69b50ff0faf286456621
Arindam Pal <M1006757@mindtree.com>
2019-05-31 09:03:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/credit', (req, res) => res.render('account', {account:accounts.credit}
 app.get('/profile', (req, res) => res.render('profile', {user:users[0]}));
 
 
-
+console.log("--------------------------------"+__dirname+'\\json\\accounts.json');
 
 
 
@@ -34,7 +34,7 @@ app.post('/transfer', (req, res) =>{
    accounts[req.body.from].balance =  accounts[req.body.from].balance - req.body.amount;
    accounts[req.body.to].balance =  parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON),'utf-8')
+    fs.writeFileSync(path.join(__dirname, '\\json\\accounts.json', accountsJSON),'utf-8')
     res.render('tranfer',{ message: 'Transfer Completed'});
 });
 
@@ -44,7 +44,7 @@ app.post('/payment', (req, res) =>{
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
      const accountsJSON = JSON.stringify(accounts, null, 4);
-     fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON),'utf-8')
+     fs.writeFileSync(path.join(__dirname, '\\json\\accounts.json', accountsJSON),'utf-8')
      res.render('payment',{message:'Payment Successfull',account:accounts.credit});
  });
 
